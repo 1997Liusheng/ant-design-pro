2d29e042c1094a7cf41c5e36cf4b39d4497b4968
993986077 <993986077@qq.com>
2018-07-07 21:39:14 +0800

    更新

--- src/index.js
+++ src/index.js
@@ -1,11 +1,10 @@
-import './polyfill';
-import dva from 'dva';
-
-import createHistory from 'history/createHashHistory';
+import './polyfill'; //Polyfill是一个js库，主要抚平不同浏览器之间对js实现的差异
+import dva from 'dva'; // 引入dva框架
+import createHistory from 'history/createHashHistory'; //http://react-guide.github.io/react-router-cn/docs/guides/basics/Histories.html
 // user BrowserHistory
 // import createHistory from 'history/createBrowserHistory';
-import createLoading from 'dva-loading';
-import 'moment/locale/zh-cn';
+import createLoading from 'dva-loading'; // https://github.com/dvajs/dva/tree/master/packages/dva-loading
+import 'moment/locale/zh-cn'; //中文语言（汉化） 456456456456
 import './rollbar';
 
 import './index.less';
