835bcd5cf915413456fadd5b69d53ec0282531e3
993986077 <993986077@qq.com>
2018-07-07 21:25:39 +0800

    test
    test

--- src/rollbar.js
+++ src/rollbar.js
@@ -2,6 +2,7 @@ import Rollbar from 'rollbar';
 
 // Track error by rollbar.com
 if (location.host === 'preview.pro.ant.design') {
+  console.log(location.host, '这是location');
   Rollbar.init({
     accessToken: '033ca6d7c0eb4cc1831cf470c2649971',
     captureUncaught: true,
